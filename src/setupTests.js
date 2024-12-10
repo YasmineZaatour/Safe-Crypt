c7a7fd6e5a4e9f920959f2f4c7af7f97edc2874d
YasmineZaatour <117782662+YasmineZaatour@users.noreply.github.com>
2024-12-10 14:27:05 +0100

    test

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,11 +1,18 @@
 // src/setupTests.js
-
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 import dotenv from 'dotenv';
 
-// Load environment variables from .env file
+// Mock Firebase
+jest.mock('firebase/app', () => ({
+  initializeApp: jest.fn(),
+}));
+
+jest.mock('firebase/auth', () => ({
+  getAuth: jest.fn(),
+}));
+
+jest.mock('firebase/firestore', () => ({
+  getFirestore: jest.fn(),
+}));
+
 dotenv.config({ path: '../.env' });
