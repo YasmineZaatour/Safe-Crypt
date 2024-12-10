b2eedee6fa9fd1c9c977e249935dd887395680c0
YasmineZaatour <117782662+YasmineZaatour@users.noreply.github.com>
2024-12-10 14:09:55 +0100

    hopefully the build of the deployment work

--- src/setupTests.js
+++ src/setupTests.js
@@ -8,4 +8,4 @@ import '@testing-library/jest-dom';
 import dotenv from 'dotenv';
 
 // Load environment variables from .env file
-dotenv.config({ path: '.env' });
+dotenv.config({ path: '../.env' });
